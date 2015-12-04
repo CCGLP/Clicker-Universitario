b340a553da9a31a910cf87e71c2a0504f93b6e6e
CCGLP <carlosgolfer@gmail.com>
2015-12-04 23:07:37 +0100

    Cambio rápido a esto porque impedia jugar en móviles con resoluciones bajas

--- html/updateWarning.js
+++ html/updateWarning.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 
   console.log("Hola, estoy funcionando")
   if (localStorage.getItem("Update") == versionActual){
-    swal("Tienes la ultima versión, gracias por testear! :) (Cada vez que se actualice el juego se reseteara a 0)")
+    swal("Tienes la ultima versión, gracias por testear! :) ")
   }
 
   else{
